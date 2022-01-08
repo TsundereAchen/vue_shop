87b554b86bf55de81b68691a05133dacdc0356cd
TsundereAchen <chenxi0x0@foxmail.com>
2022-01-08 18:46:48 +0800

    数据报表功能，安装echarts依赖

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import Params from '../components/goods/Params'
 import Goods from '../components/goods/Goods'
 import Add from '../components/goods/Add'
 import Orders from '../components/orders/Orders'
+import Reports from '../components/reports/Reports'
 
 Vue.use(VueRouter)
 
@@ -28,6 +29,7 @@ const routes = [
       { path: '/goods', component: Goods },
       { path: '/goods/add', component: Add },
       { path: '/orders', component: Orders },
+      { path: '/reports', component: Reports },
     ]
   },
 ]
